0dd4dd5fd09e3095591f5c28db6da2d31e2cf0f7
noahfschr <noah@upstash.com>
2021-03-16 12:54:40 -0700

    update conn count for free

--- constants/index.js
+++ constants/index.js
@@ -45,7 +45,7 @@ export const PRICES = {
     title: 'Max Concurrent Connections',
     description:
       'This is the maximum allowed number of concurrent connections (clients) at a moment. This cap does not essentially limit the number of requests that can be submitted per second, but defines the maximum number of open TCP connections to the database.',
-    free: '20',
+    free: '100',
     standard: '1000',
     premium: '10,000'
   },
