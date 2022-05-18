f9391b4051ebbacbc366aaefd017e4a0e62c06e3
Adem ilter <ademilter@gmail.com>
2022-05-18 19:25:30 +0300

    post clap (#16)
    * clap
    
    * fix env
    
    * refactor
    
    * fix max clap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,19 @@
-import "../styles/app.css";
-import "../styles/prism.css";
+import "styles/app.css";
+import "styles/prism.css";
 import "react-typist/dist/Typist.css";
 
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
 import Head from "next/head";
-import FontFace from "components/font-face";
 import Footer from "components/footer";
 import Header from "components/header";
 import MobileMenu from "components/mobile-menu";
 import * as gtag from "lib/gtag";
 import { META } from "constants/";
 import theme from "theme";
-import type { AppProps, NextWebVitalsMetric } from "next/app";
-
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric);
-}
+import { Provider, CachePolicies } from "use-http";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -36,70 +32,86 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     };
   }, []);
 
+  const options = {
+    cachePolicy: CachePolicies.NO_CACHE,
+    interceptors: {
+      request: async ({ options, url, path, route }) => {
+        return options;
+      },
+      response: async ({ response }) => {
+        return response;
+      },
+    },
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <Provider url={`${process.env.NEXT_PUBLIC_URL}/api`} options={options}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
 
-        {/* base */}
-        <title key="title">{META.title}</title>
-        <meta key="description" name="description" content={META.description} />
-        <link key="canonical" rel="canonical" href={META.url} />
+          {/* base */}
+          <title key="title">{META.title}</title>
+          <meta
+            key="description"
+            name="description"
+            content={META.description}
+          />
+          <link key="canonical" rel="canonical" href={META.url} />
 
-        {/* facebook */}
-        <meta key="og:url" property="og:url" content={META.url} />
-        <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:title" property="og:title" content={META.title} />
-        <meta
-          key="og:image"
-          property="og:image"
-          content={META.coverImagePath}
-        />
-        <meta
-          key="og:description"
-          property="og:description"
-          content={META.description}
-        />
+          {/* facebook */}
+          <meta key="og:url" property="og:url" content={META.url} />
+          <meta key="og:type" property="og:type" content="website" />
+          <meta key="og:title" property="og:title" content={META.title} />
+          <meta
+            key="og:image"
+            property="og:image"
+            content={META.coverImagePath}
+          />
+          <meta
+            key="og:description"
+            property="og:description"
+            content={META.description}
+          />
 
-        {/* twitter */}
-        <meta
-          key="twitter:card"
-          name="twitter:card"
-          content="summary_large_image"
-        />
-        <meta key="twitter:url" name="twitter:url" content={META.url} />
-        <meta
-          key="twitter:site"
-          name="twitter:site"
-          content={META.twitterAccount}
-        />
-        <meta
-          key="twitter:author"
-          name="twitter:author"
-          content={META.twitterAccount}
-        />
-        <meta key="twitter:title" name="twitter:title" content={META.title} />
-        <meta
-          key="twitter:image"
-          name="twitter:image"
-          content={META.coverImagePath}
-        />
-        <meta
-          key="twitter:description"
-          name="twitter:description"
-          content={META.description}
-        />
-      </Head>
+          {/* twitter */}
+          <meta
+            key="twitter:card"
+            name="twitter:card"
+            content="summary_large_image"
+          />
+          <meta key="twitter:url" name="twitter:url" content={META.url} />
+          <meta
+            key="twitter:site"
+            name="twitter:site"
+            content={META.twitterAccount}
+          />
+          <meta
+            key="twitter:author"
+            name="twitter:author"
+            content={META.twitterAccount}
+          />
+          <meta key="twitter:title" name="twitter:title" content={META.title} />
+          <meta
+            key="twitter:image"
+            name="twitter:image"
+            content={META.coverImagePath}
+          />
+          <meta
+            key="twitter:description"
+            name="twitter:description"
+            content={META.description}
+          />
+        </Head>
 
-      <FontFace />
-
-      <Header onOpen={onOpen} />
-      <MobileMenu isOpen={isOpen} onClose={onClose} />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+        <Header onOpen={onOpen} />
+        <MobileMenu isOpen={isOpen} onClose={onClose} />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </Provider>
     </ChakraProvider>
   );
 }
---
+++ pages/api/post/clap/[id].ts
@@ -0,0 +1,48 @@
+import redis from "lib/redis";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { MAX_CLAP } from "constants/index";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const method = req.method;
+  const { id } = req.query;
+
+  try {
+    if (method === "GET") {
+      const count = await redis.get(`post:${id}`);
+      console.log(count);
+      return res.status(200).json({ count: count || 0 });
+    }
+
+    if (method === "PATCH") {
+      const ip =
+        req.headers["x-real-ip"] ||
+        req.headers["x-forwarded-for"] ||
+        req.headers["Remote_Addr"] ||
+        "NA";
+
+      const response =
+        ip === "NA" ? 1 : await redis.sadd(`vote:post:${id}`, ip.toString());
+
+      if (response === 0) {
+        return res.status(200).json({ message: "Already voted" });
+      }
+
+      const initialCount = req.body.count || 1;
+
+      const count = await redis.incrby(
+        `post:${id}`,
+        initialCount > MAX_CLAP ? MAX_CLAP : initialCount
+      );
+      return res.status(200).json({ count });
+    }
+
+    return res.status(400).json({ message: "Method not allowed" });
+  } catch (e) {
+    res.status(500).json({
+      error: e.message,
+    });
+  }
+}
