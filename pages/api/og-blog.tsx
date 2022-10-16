f7162681472d046c724f7e1f5862b1e53db584b5
Adem ilter <ademilter@gmail.com>
2022-10-16 13:54:25 +0300

    fix next bug

--- pages/api/og-blog.tsx
+++ pages/api/og-blog.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "@vercel/og";
-import { NextRequest } from "next/server";
 import OGBlog from "components/og-blog";
 import authors from "../../authors";
+import type { NextRequest } from "next/server";
 
 export const config = {
   runtime: "experimental-edge",
