1494e7e6a487a1fd1f9c0e76c0d83e32d909d302
Adem ilter <ademilter@gmail.com>
2022-11-07 09:35:30 +0300

    fix CONSL-260

--- components/redis-pricing-desktop/col-tooltip.tsx
+++ components/redis-pricing-desktop/col-tooltip.tsx
@@ -1,11 +1,19 @@
-import { Box, Tooltip } from "@chakra-ui/react";
+import { Box, Tooltip, Text } from "@chakra-ui/react";
 import * as Icon from "../icons";
 
 export default function CustomTooltip({ children }) {
   if (!children) return null;
 
   return (
-    <Tooltip shouldWrapChildren placement="top" label={children}>
+    <Tooltip
+      shouldWrapChildren
+      placement="top"
+      label={
+        <Text color="gray.900" p={2}>
+          {children}
+        </Text>
+      }
+    >
       <Box
         as={Icon.Info}
         ml={1}
