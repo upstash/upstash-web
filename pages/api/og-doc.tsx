962f615271db2bc6171af07bc4bedd5398760984
Adem ilter <ademilter@gmail.com>
2023-01-12 12:05:20 +0300

    fix doc og image

--- pages/api/og-doc.tsx
+++ pages/api/og-doc.tsx
@@ -7,10 +7,10 @@ export const config = {
 };
 
 const FontInterRegular = fetch(
-  new URL("../../public/static/fonts/Inter-Regular.ttf", import.meta.url)
+  new URL("../../../public/static/fonts/Inter-Regular.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 const FontInterBold = fetch(
-  new URL("../../public/static/fonts/Inter-Bold.ttf", import.meta.url)
+  new URL("../../../public/static/fonts/Inter-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler(req: NextRequest) {
@@ -18,37 +18,74 @@ export default async function handler(req: NextRequest) {
   const DataInterBold = await FontInterBold;
 
   try {
-    const { searchParams } = new URL(req.url);
+    const docsUrl = new URL(req.url).searchParams.get("url");
 
-    const title = searchParams.get("title");
-    const path = searchParams.get("path");
-
-    if (!title || !path) {
-      throw new Error("Missing required query parameters");
+    if (!docsUrl) {
+      throw new Error("url parameter must be set");
     }
 
-    return new ImageResponse(<OGDoc title={title} product={path} />, {
-      // debug: true,
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: "Inter",
-          data: DataInterRegular,
-          style: "normal",
-          weight: 400,
+    const title = await getPageTitle(docsUrl);
+
+    return new ImageResponse(
+      (
+        <OGDoc
+          title={title}
+          product={new URL(docsUrl).pathname.replace(/^\//, "")}
+        />
+      ),
+      {
+        // debug: true,
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Inter",
+            data: DataInterRegular,
+            style: "normal",
+            weight: 400,
+          },
+          {
+            name: "Inter",
+            data: DataInterBold,
+            style: "normal",
+            weight: 800,
+          },
+        ],
+        headers: {
+          "Cache-Control": "s-maxage=3600",
         },
-        {
-          name: "Inter",
-          data: DataInterBold,
-          style: "normal",
-          weight: 800,
-        },
-      ],
-    });
+      }
+    );
   } catch (e: any) {
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
+  }
+}
+
+/**
+ * Fetch the docs page and parse its content to get the page title
+ *
+ */
+async function getPageTitle(url: string): Promise<string> {
+  const fallbackTitle = "Upstash Documentation";
+
+  try {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 2000);
+    const res = await fetch(url, { signal: controller.signal });
+    clearTimeout(timeout);
+    const body = await res.text();
+
+    const titleMatch = body.match(/<title.*>([^<]*)<\/title>/);
+    if (!titleMatch || typeof titleMatch[1] !== "string") {
+      console.error("No valid title found", url);
+      return fallbackTitle;
+    }
+
+    return titleMatch[1].replace(" | Upstash: Documentation", "");
+  } catch (err) {
+    console.log(err);
+    return fallbackTitle;
   }
 }
