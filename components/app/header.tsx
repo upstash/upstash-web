73024f334a4b9736873b289679023e81de2a9da2
Adem ilter <ademilter@gmail.com>
2023-04-27 12:24:44 +0300

    fix navigation

--- components/app/header.tsx
+++ components/app/header.tsx
@@ -2,9 +2,9 @@ import { HTMLAttributes } from "react";
 import cx from "@/utils/cx";
 import { Logo } from "@/components/app/logo";
 import Link from "next/link";
-import { allJobs } from "contentlayer/generated";
 import Button from "@/components/button";
 import Container from "@/components/container";
+import Nav from "./nav";
 
 export interface IAppHeader extends HTMLAttributes<HTMLDivElement> {}
 
@@ -20,45 +20,7 @@ export default function Header({ className, ...props }: IAppHeader) {
           </div>
 
           {/* nav */}
-          <nav
-            className="col-span-2 flex items-center justify-center gap-6
-          opacity-60 transition group-hover:opacity-100"
-          >
-            <Link
-              href="/about"
-              className="hover:text-emerald-300 hover:underline"
-            >
-              Pricing
-            </Link>
-            {/*<Link href="/about">Open Source</Link>*/}
-            <Link
-              href="/about"
-              className="hover:text-emerald-300 hover:underline"
-            >
-              About
-            </Link>
-            <Link
-              href="/blog"
-              className="hover:text-emerald-300 hover:underline"
-            >
-              Blog
-            </Link>
-            <Link
-              href="/careers"
-              className="hover:text-emerald-300 hover:underline"
-            >
-              Careers
-              <span
-                className="ml-0.5 rounded-full bg-emerald-300/20
-              px-1.5 py-0.5 font-mono text-sm text-emerald-500"
-              >
-                {allJobs.filter((o) => !o.draft).length}
-              </span>
-            </Link>
-            <Button href="https://docs.upstash.com" target="_blank">
-              Docs
-            </Button>
-          </nav>
+          <Nav />
 
           {/* cta */}
           <div className="flex justify-end">
---
+++ components/app/nav.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { HTMLAttributes, ReactNode } from "react";
+import Link, { LinkProps } from "next/link";
+import { allJobs } from "contentlayer/generated";
+import Button from "@/components/button";
+import { usePathname } from "next/navigation";
+import cx from "@/utils/cx";
+
+export default function Nav({}: HTMLAttributes<HTMLDivElement> & {}) {
+  return (
+    <nav className="col-span-2 flex items-center justify-center gap-2">
+      {[
+        {
+          name: "Pricing",
+          href: "/#pricing",
+        },
+        // {
+        //   name: "Open Source",
+        //   href: "/open-source",
+        // },
+        {
+          name: "About",
+          href: "/about",
+        },
+        {
+          name: "Blog",
+          href: "/blog",
+        },
+        {
+          name: "Careers",
+          href: "/careers",
+          children: (
+            <span
+              className="ml-0.5 rounded-full bg-emerald-300/20
+              px-1.5 py-0.5 font-mono text-sm text-emerald-500"
+            >
+              {allJobs.filter((o) => !o.draft).length}
+            </span>
+          ),
+        },
+      ].map((item) => {
+        return (
+          <NavLink key={item.href} href={item.href}>
+            {item.name}
+            {item.children}
+          </NavLink>
+        );
+      })}
+      <Button href="https://docs.upstash.com" target="_blank">
+        Docs
+      </Button>
+    </nav>
+  );
+}
+
+function NavLink({
+  href,
+  className,
+  children,
+  ...props
+}: LinkProps & { className?: string; children: ReactNode }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={cx(
+        "rounded-full px-4 py-2 text-zinc-400 transition",
+        "hover:bg-white/5",
+        isActive && "!bg-white/10 !text-zinc-50",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
