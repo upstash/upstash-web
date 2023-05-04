e6c4deb8ca3e834c0c10431f686a17fc37bac9cb
Adem ilter <ademilter@gmail.com>
2023-05-04 10:07:58 +0300

    fix globe

--- components/home/fast/index.tsx
+++ components/home/fast/index.tsx
@@ -10,14 +10,13 @@ import Bg from "@/components/bg";
 
 export default function Fast() {
   return (
-    <section className="relative z-0 mt-10 hidden h-[700px] overflow-hidden md:block">
-      {/*  */}
+    <section className="relative z-0 mt-10 hidden h-[600px] overflow-hidden md:block">
       <Bg className="top-44 h-1/2" />
 
       {/* slogan */}
       <h5
         className={cx(
-          "absolute inset-x-0 top-[40px] -z-10 select-none",
+          "pointer-events-none absolute inset-x-0 top-[40px] -z-10",
           "font-display text-[13vw] font-bold leading-none",
           "bg-gradient-to-t bg-clip-text text-transparent",
           "from-emerald-200/5 to-emerald-100/[0.02]"
@@ -27,14 +26,14 @@ export default function Fast() {
       </h5>
 
       {/* globe */}
-      <div className="absolute -top-0 left-1/2 z-0 -translate-x-1/2">
+      <div className="absolute -top-[180px] left-1/2 z-0 -translate-x-1/2">
         <Globe />
       </div>
 
       {/* bottom-bg */}
       <div
-        className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-[200px]
-      bg-gradient-to-b from-transparent to-zinc-950"
+        className="pointer-events-none absolute inset-x-0 bottom-0 z-10
+      h-[200px] bg-gradient-to-b from-transparent to-zinc-950"
       />
 
       {/* data */}
