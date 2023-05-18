edf4b3c563682e5fb46909710ceb445db8aa0aae
Adem ilter <ademilter@gmail.com>
2023-05-18 23:56:32 +0300

    fix: delete sourcemap

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // https://upstash.slack.com/archives/C02Q9BA1F8C/p1684349628476519
-  productionBrowserSourceMaps: true,
   env: {
     NEXT_PUBLIC_SITE_URL:
       process.env.NODE_ENV === "development"
