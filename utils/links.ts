ced52bae658a59c4d97ecd452d59980a64ff400b
Adem ilter <ademilter@gmail.com>
2023-05-18 23:59:28 +0300

    fix: globe (use cobe) (#65)
    * fix: globe
    
    * style: fast globe
    
    * fix: delete sourcemap
    
    * fix: scroll open-source

--- utils/links.ts
+++
@@ -1,6 +0,0 @@
-export const SOCIAL_LINKS = {
-  blog: "https://blog.upstash.com",
-  medium: "https://medium.com/upstash",
-  twitter: "https://twitter.com/upstash",
-  discord: "https://discord.gg/w9SenAtbme",
-};
--- utils/number-format.ts
+++
@@ -1,8 +0,0 @@
-export function numberFormat(
-  value: number,
-  options?: Intl.NumberFormatOptions
-) {
-  return Intl.NumberFormat(undefined, { notation: options?.notation }).format(
-    value
-  );
-}
