ecf0580e013bfb23287a6da780982ed4b0f4d12f
Adem ilter <ademilter@gmail.com>
2023-05-19 12:04:26 +0300

    fix: next update

--- components/home/section-menu.tsx
+++ components/home/section-menu.tsx
@@ -72,6 +72,7 @@ export default function SectionMenu({
         )}
       >
         <SpyLink
+          to={""}
           onClick={() =>
             animateScroll.scrollToTop({
               duration: 0,
