c6756f8c145606120815fddd74c559009d89312e
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-08-15 22:00:36 +0300

    fix: example api

--- app/examples/get-data.ts
+++ app/examples/get-data.ts
@@ -1,8 +1,7 @@
 import { Example } from "@/utils/type";
 
 export default async function getData(): Promise<Example[]> {
-  // https://upstash-examples-content.vercel.app/
-  return await fetch("http://localhost:3001")
+  https: return await fetch("https://upstash-examples-content.vercel.app/")
     .then((res) => res.json())
     .catch((err) => {
       console.log(err);
