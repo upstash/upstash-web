0d0a465247f2e804c1befcfbaa097ef63c97d7b3
chronark <dev@chronark.com>
2023-10-02 19:11:21 +0200

    fix(get-data.ts): handle null response from fetch by returning an empty array

--- app/examples/get-data.ts
+++ app/examples/get-data.ts
@@ -2,7 +2,7 @@ import { Example } from "@/utils/type";
 
 export default async function getData(): Promise<Example[]> {
   return await fetch("https://upstash-examples-content.vercel.app/")
-    .then((res) => res.json())
+    .then((res) => res.json() ?? [])
     .catch((err) => {
       console.log(err);
       return [];
