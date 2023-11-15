c44483e26af43df1c8aa6a6d937bbb53bfb875fa
Fahreddin Özcan <88107904+fahreddinozcan@users.noreply.github.com>
2023-11-15 12:27:46 +0300

    DX-436 Intercom - GTM Integration (#150)
    * add: gtag management
    
    * test to deploy
    
    * remove: hubspot
    
    * edit: credential source

---
+++ lib/google/gtm.tsx
@@ -0,0 +1,22 @@
+type WindowWithDataLayer = Window & {
+  dataLayer: Record<string, any>[];
+};
+
+declare const window: WindowWithDataLayer;
+
+export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+
+export const pageview = (url: string) => {
+  if (typeof window.dataLayer !== "undefined") {
+    console.log("Working");
+    window.dataLayer.push({
+      event: "pageview",
+      page: url,
+    });
+  } else {
+    console.log({
+      event: "pageview",
+      page: url,
+    });
+  }
+};
