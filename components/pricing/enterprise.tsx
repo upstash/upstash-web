682a13ece664105314b92d1492dcf82d90388b17
Fahreddin Özcan <88107904+fahreddinozcan@users.noreply.github.com>
2023-11-23 21:41:24 +0300

    DX-488 pricing page update for kafka section (#157)
    * DX-488: fix kafka section pricing platform data
    
    * change role to title

--- components/pricing/enterprise.tsx
+++ components/pricing/enterprise.tsx
@@ -4,11 +4,7 @@ import Button from "@/components/button";
 
 export default function PricingTableEnterprise() {
   return (
-    <div
-      className="items-center rounded-3xl border-2 border-emerald-300/10 bg-emerald-300/3
-    px-10 py-8 text-left
-    text-emerald-100 md:mx-32 md:flex md:px-16 md:py-10"
-    >
+    <div className="items-center rounded-3xl border-2 border-emerald-300/10 bg-emerald-300/3 px-10 py-8 text-left text-emerald-100 md:mx-32 md:flex md:px-16 md:py-10">
       <div className="grow">
         <h4 className="text-2xl font-semibold text-emerald-400">Enterprise</h4>
         <p className="">For businesses with advanced needs.</p>
@@ -63,6 +59,7 @@ const Icon = () => (
     strokeLinecap="round"
     strokeLinejoin="round"
   >
+    <title> </title>
     <path
       d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z"
       strokeWidth="0"
