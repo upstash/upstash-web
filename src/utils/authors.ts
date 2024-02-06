0337cd8578c9369d96ad58c796e684a6ffbc6e8e
brightonmboya <b.mboya@alustudent.com>
2024-02-06 11:00:14 +0200

    saas blog

--- src/utils/authors.ts
+++ src/utils/authors.ts
@@ -1,307 +1,315 @@
-export const authors: Record<string,
-    {
-        name: string;
-        title: string;
-        image: string;
-        url?: string;
-    }> = {
-    lisa: {
-        name: "Lisa Natsumi",
-        title: "Engineer @Upstash",
-        image: "pp-lisa.jpg",
-    },
-    enes: {
-        name: "Enes Akar",
-        title: "Cofounder @Upstash",
-        url: "https://twitter.com/enesakar",
-        image: "enes.png",
-    },
-    noah: {
-        name: "Noah Fischer",
-        title: "DevRel @Upstash",
-        url: "https://twitter.com/noahfschr",
-        image: "noah.jpg",
-    },
-    mehmet: {
-        name: "Mehmet Dogan",
-        title: "CTO @Upstash",
-        url: "https://twitter.com/mmdogan",
-        image: "mehmet.jpg",
-    },
-    oguzhan: {
-        name: "Oguzhan Olguncu",
-        title: "Software Engineer @Upstash",
-        url: "https://ogzhanolguncu.com",
-        image: "oguzhan.jpeg",
-    },
-    sancar: {
-        name: "Sancar Koyunlu",
-        title: "Senior Software Engineer @Upstash",
-        image: "sancar.jpg",
-    },
-    geoff: {
-        name: "Geoff Rich",
-        title: " Svelte core team member (Guest Author)",
-        url: "https://geoffrich.net/",
-        image: "geoff.jpg",
-    },
-    rosius: {
-        name: "Rosius Ndimofor",
-        title: "AWS Serverless Hero (Guest Author)",
-        url: "https://twitter.com/atehrosius",
-        image: "rosius.png",
-    },
-    krutie: {
-        name: "Krutie Patel",
-        title: "NuxtJS Ambassador (Guest Author)",
-        url: "https://mobile.twitter.com/KrutiePatel",
-        image: "krutie.jpeg",
-    },
-    omer: {
-        name: "Omer Aytac",
-        title: "Full Stack Developer (Guest Author)",
-        url: "https://mobile.twitter.com/OmerAytac_",
-        image: "omer.png",
-    },
-    leigh: {
-        name: "Leigh Halliday",
-        title: "Senior Engineering Manager, Wrapbook (Guest Author)",
-        url: "https://www.youtube.com/user/leighhalliday",
-        image: "leigh.png",
-    },
-    gabriel: {
-        name: "Gabriel Guy",
-        title: "Head of Product, Applicaster (Guest Author)",
-        url: "https://twitter.com/gavriguy",
-        image: "gabriel.png",
-    },
-    sonke: {
-        name: "Sönke Peters",
-        title: "Full Stack Web Developer (Guest Author)",
-        url: "https://www.soenkep.com/",
-        image: "sonke.jpg",
-    },
-    kay: {
-        name: "Kay Plößer",
-        title: "Developer and Author (Guest Author)",
-        url: "https://twitter.com/K4y1s",
-        image: "kay.jpeg",
-    },
-    adem: {
-        name: "Adem Ilter",
-        title: "Designer @Upstash",
-        url: "https://twitter.com/ademilter",
-        image: "adem.jpg",
-    },
-    ilter: {
-        name: "Ilter Kavlak",
-        title: "Site Reliability Engineer @Upstash",
-        url: "https://twitter.com/ilterkavlak",
-        image: "ilter.png",
-    },
-    burak: {
-        name: "Burak Yılmaz",
-        title: "Site Reliability Engineer @Upstash",
-        url: "https://github.com/yilmaz-burak",
-        image: "burak.png",
-    },
-    andreas: {
-        name: "Andreas Thomas",
-        title: "Software Engineer @Upstash",
-        url: "https://twitter.com/chronark_",
-        image: "andreas.jpeg",
-    },
-    vedant: {
-        name: "Vedant Atodaria",
-        title: "Frontend Engineer (Guest Author)",
-        url: "https://fish.solar/",
-        image: "fish.jpeg",
-    },
-    nihan: {
-        name: "Nihan Iman",
-        title: "Software Developer (Guest Author)",
-        image: "nihan2.png",
-    },
-    bobby: {
-        name: "Bobby Iliev",
-        title: "DevOps Engineer (Guest Author)",
-        image: "bobby.jpg",
-    },
-    jake: {
-        name: "Jake Lynch",
-        title: "CTO, Fig (Guest Author)",
-        image: "jake.jpg",
-    },
-    vladimir: {
-        name: "Vladimir Mihailenco",
-        title: "Uptrace Founder, Go-Redis Author",
-        image: "vladimir.jpg",
-    },
-    rodney: {
-        name: "Rodney Johnson",
-        title: "Technical Content Writer",
-        image: "rodney.jpg",
-    },
-    lorenzo: {
-        name: "Lorenzo Noccioli",
-        title: "Founder at popEating",
-        image: "lorenzo.jpg",
-    },
-    lambros: {
-        name: "Lambros Petrou",
-        title: "Software Engineer at Datadog (Guest Author)",
-        image: "lambros.jpg",
-    },
-    max: {
-        name: "Maximilian Kaske",
-        title: "Fullstack Developer (Guest Author)",
-        url: "https://twitter.com/mxkaske",
-        image: "max.jpg",
-    },
-    menelaos: {
-        name: "Menelaos Kotsollaris",
-        title: "Staff Software Engineer at Realtor.com (Guest Author)",
-        url: "https://menelaos.dev/",
-        image: "menelaos.jpeg",
-    },
-    enesozcan: {
-        name: "Enes Ozcan",
-        title: "Senior Software Engineer at Zapata Computing (Guest Author)",
-        url: "https://github.com/enozcan",
-        image: "enesozcan.png",
-    },
-    tudor: {
-        name: "Tudor Zgîmbău",
-        title: "Software Engineer (Guest Author)",
-        url: "https://www.linkedin.com/in/tudor-zg%C3%AEmb%C4%83u-a85274234",
-        image: "tudor.jpg",
-    },
-    chillyfish: {
-        name: "Chillyfish",
-        title: "Full-Stack Engineer (Guest Author)",
-        url: "https://twitter.com/chillyfish_dev",
-        image: "chillyfish.jpg",
-    },
-    andreaselia: {
-        name: "Andreas Elia",
-        title: "Full-Stack Engineer (Guest Author)",
-        url: "https://twitter.com/digitalandreas",
-        image: "andreaselia.jpeg",
-    },
-    bilaltahir: {
-        name: "Bilal Tahir",
-        title: "Front-end developer (Guest Author)",
-        url: "https://twitter.com/deepwhitman",
-        image: "bilaltahir.jpeg",
-    },
-    thomasCazade: {
-        name: "Thomas Cazade",
-        title: "Founder of fastsheet (Guest Author)",
-        url: "https://twitter.com/TotomInc",
-        image: "thomas_cazade.jpeg",
-    },
-    russTran: {
-        name: "Russ Tran",
-        title: "Frontend Engineer at JUST.engineer (Guest Author)",
-        url: "https://twitter.com/russdevdotnet",
-        image: "russ_tran.jpeg",
-    },
-    hosna: {
-        name: "Hosna Qasmei",
-        title: "Full Stack Developer (Guest Author)",
-        url: "https://twitter.com/hqasmei",
-        image: "hosna.jpeg",
-    },
-    fahreddin: {
-        name: "Fahreddin Ozcan",
-        title: "Software Engineer @Upstash",
-        url: "https://github.com/fahreddinozcan",
-        image: "fahreddin.png",
-    },
-    rishi: {
-        name: "Rishi Raj Jain",
-        title: "Founder of LaunchFa.st (Guest Author)",
-        image: "rishi.jpeg",
-    },
-    pau_kraft: {
-        name: "Pau Kraft",
-        title: "Software Engineer @TeampilotAI (Guest Author)",
-        image: "pau_kraft.png",
-    },
-    jaydeep: {
-        name: "Jaydeep Das",
-        title: "Software Developer (Guest Author)",
-        image: "jaydeep.jpeg",
-    },
-    chris: {
-        name: "Chris Jayden",
-        title: "Software Engineer (Guest Author)",
-        image: "chris.jpg",
-    },
-    anshuman: {
-        name: "Anshuman Bhardwaj",
-        title: "Founder @docsly (Guest Author)",
-        image: "anshuman.png",
-    },
-    anish: {
-        name: "Anish Pallati",
-        title: "Software Developer (Guest Author)",
-        image: "anish.jpeg",
-    },
-    cameron: {
-        name: "Cameron Youngblood",
-        title: "Software Developer (Guest Author)",
-        image: "cameron.jpeg",
-    },
-    asim: {
-        name: "Asim Hafeez",
-        title: "Software Developer (Guest Author)",
-        image: "asim.jpeg",
-    },
-    brace: {
-        name: "Brace Sproul",
-        title: "Software Developer (Guest Author)",
-        image: "brace.jpeg",
-    },
-    ruben: {
-        name: "Ruben de Groot",
-        title: "Software Developer (Guest Author)",
-        image: "ruben.jpeg",
-    },
-    meshan: {
-        name: "Meshan Khosla",
-        title: "Software Engineer",
-        image: "meshan.png",
-    },
-    murillo: {
-        name: "Murillo de Miranda",
-        title: "Software Developer (Guest Author)",
-        image: "murillo.png",
-    },
-    yusuf: {
-        name: "Yusuf Kimirti",
-        title: "Sofware Engineer @Upstash",
-        image: "yusuf.jpg",
-    },
-    valon: {
-        name: "Valon Januzaj",
-        title: "Software Developer (Guest Author)",
-        image: "valon.png",
-    },
-    mahesh: {
-        name: "Mahesh Saini",
-        title: "Software Developer (Guest Author)",
-        image: "mahesh.jpeg",
-    },
-    eray: {
-        name: "Eray Eroglu",
-        title: "Software Developer (Guest Author)",
-        image: "eray.jpeg",
-    },
-    oguzhantasimaz: {
-        name: "Oguzhan Tasimaz",
-        title: "Software Engineer",
-        url: "https://oguzhantasimaz.com",
-        image: "oguzhantasimaz.png",
-    }
+export const authors: Record<
+  string,
+  {
+    name: string;
+    title: string;
+    image: string;
+    url?: string;
+  }
+> = {
+  lisa: {
+    name: "Lisa Natsumi",
+    title: "Engineer @Upstash",
+    image: "pp-lisa.jpg",
+  },
+  enes: {
+    name: "Enes Akar",
+    title: "Cofounder @Upstash",
+    url: "https://twitter.com/enesakar",
+    image: "enes.png",
+  },
+  noah: {
+    name: "Noah Fischer",
+    title: "DevRel @Upstash",
+    url: "https://twitter.com/noahfschr",
+    image: "noah.jpg",
+  },
+  mehmet: {
+    name: "Mehmet Dogan",
+    title: "CTO @Upstash",
+    url: "https://twitter.com/mmdogan",
+    image: "mehmet.jpg",
+  },
+  oguzhan: {
+    name: "Oguzhan Olguncu",
+    title: "Software Engineer @Upstash",
+    url: "https://ogzhanolguncu.com",
+    image: "oguzhan.jpeg",
+  },
+  sancar: {
+    name: "Sancar Koyunlu",
+    title: "Senior Software Engineer @Upstash",
+    image: "sancar.jpg",
+  },
+  geoff: {
+    name: "Geoff Rich",
+    title: " Svelte core team member (Guest Author)",
+    url: "https://geoffrich.net/",
+    image: "geoff.jpg",
+  },
+  rosius: {
+    name: "Rosius Ndimofor",
+    title: "AWS Serverless Hero (Guest Author)",
+    url: "https://twitter.com/atehrosius",
+    image: "rosius.png",
+  },
+  krutie: {
+    name: "Krutie Patel",
+    title: "NuxtJS Ambassador (Guest Author)",
+    url: "https://mobile.twitter.com/KrutiePatel",
+    image: "krutie.jpeg",
+  },
+  omer: {
+    name: "Omer Aytac",
+    title: "Full Stack Developer (Guest Author)",
+    url: "https://mobile.twitter.com/OmerAytac_",
+    image: "omer.png",
+  },
+  leigh: {
+    name: "Leigh Halliday",
+    title: "Senior Engineering Manager, Wrapbook (Guest Author)",
+    url: "https://www.youtube.com/user/leighhalliday",
+    image: "leigh.png",
+  },
+  gabriel: {
+    name: "Gabriel Guy",
+    title: "Head of Product, Applicaster (Guest Author)",
+    url: "https://twitter.com/gavriguy",
+    image: "gabriel.png",
+  },
+  sonke: {
+    name: "Sönke Peters",
+    title: "Full Stack Web Developer (Guest Author)",
+    url: "https://www.soenkep.com/",
+    image: "sonke.jpg",
+  },
+  kay: {
+    name: "Kay Plößer",
+    title: "Developer and Author (Guest Author)",
+    url: "https://twitter.com/K4y1s",
+    image: "kay.jpeg",
+  },
+  adem: {
+    name: "Adem Ilter",
+    title: "Designer @Upstash",
+    url: "https://twitter.com/ademilter",
+    image: "adem.jpg",
+  },
+  ilter: {
+    name: "Ilter Kavlak",
+    title: "Site Reliability Engineer @Upstash",
+    url: "https://twitter.com/ilterkavlak",
+    image: "ilter.png",
+  },
+  burak: {
+    name: "Burak Yılmaz",
+    title: "Site Reliability Engineer @Upstash",
+    url: "https://github.com/yilmaz-burak",
+    image: "burak.png",
+  },
+  andreas: {
+    name: "Andreas Thomas",
+    title: "Software Engineer @Upstash",
+    url: "https://twitter.com/chronark_",
+    image: "andreas.jpeg",
+  },
+  vedant: {
+    name: "Vedant Atodaria",
+    title: "Frontend Engineer (Guest Author)",
+    url: "https://fish.solar/",
+    image: "fish.jpeg",
+  },
+  nihan: {
+    name: "Nihan Iman",
+    title: "Software Developer (Guest Author)",
+    image: "nihan2.png",
+  },
+  bobby: {
+    name: "Bobby Iliev",
+    title: "DevOps Engineer (Guest Author)",
+    image: "bobby.jpg",
+  },
+  jake: {
+    name: "Jake Lynch",
+    title: "CTO, Fig (Guest Author)",
+    image: "jake.jpg",
+  },
+  vladimir: {
+    name: "Vladimir Mihailenco",
+    title: "Uptrace Founder, Go-Redis Author",
+    image: "vladimir.jpg",
+  },
+  rodney: {
+    name: "Rodney Johnson",
+    title: "Technical Content Writer",
+    image: "rodney.jpg",
+  },
+  lorenzo: {
+    name: "Lorenzo Noccioli",
+    title: "Founder at popEating",
+    image: "lorenzo.jpg",
+  },
+  lambros: {
+    name: "Lambros Petrou",
+    title: "Software Engineer at Datadog (Guest Author)",
+    image: "lambros.jpg",
+  },
+  max: {
+    name: "Maximilian Kaske",
+    title: "Fullstack Developer (Guest Author)",
+    url: "https://twitter.com/mxkaske",
+    image: "max.jpg",
+  },
+  menelaos: {
+    name: "Menelaos Kotsollaris",
+    title: "Staff Software Engineer at Realtor.com (Guest Author)",
+    url: "https://menelaos.dev/",
+    image: "menelaos.jpeg",
+  },
+  enesozcan: {
+    name: "Enes Ozcan",
+    title: "Senior Software Engineer at Zapata Computing (Guest Author)",
+    url: "https://github.com/enozcan",
+    image: "enesozcan.png",
+  },
+  tudor: {
+    name: "Tudor Zgîmbău",
+    title: "Software Engineer (Guest Author)",
+    url: "https://www.linkedin.com/in/tudor-zg%C3%AEmb%C4%83u-a85274234",
+    image: "tudor.jpg",
+  },
+  chillyfish: {
+    name: "Chillyfish",
+    title: "Full-Stack Engineer (Guest Author)",
+    url: "https://twitter.com/chillyfish_dev",
+    image: "chillyfish.jpg",
+  },
+  andreaselia: {
+    name: "Andreas Elia",
+    title: "Full-Stack Engineer (Guest Author)",
+    url: "https://twitter.com/digitalandreas",
+    image: "andreaselia.jpeg",
+  },
+  bilaltahir: {
+    name: "Bilal Tahir",
+    title: "Front-end developer (Guest Author)",
+    url: "https://twitter.com/deepwhitman",
+    image: "bilaltahir.jpeg",
+  },
+  thomasCazade: {
+    name: "Thomas Cazade",
+    title: "Founder of fastsheet (Guest Author)",
+    url: "https://twitter.com/TotomInc",
+    image: "thomas_cazade.jpeg",
+  },
+  russTran: {
+    name: "Russ Tran",
+    title: "Frontend Engineer at JUST.engineer (Guest Author)",
+    url: "https://twitter.com/russdevdotnet",
+    image: "russ_tran.jpeg",
+  },
+  hosna: {
+    name: "Hosna Qasmei",
+    title: "Full Stack Developer (Guest Author)",
+    url: "https://twitter.com/hqasmei",
+    image: "hosna.jpeg",
+  },
+  fahreddin: {
+    name: "Fahreddin Ozcan",
+    title: "Software Engineer @Upstash",
+    url: "https://github.com/fahreddinozcan",
+    image: "fahreddin.png",
+  },
+  rishi: {
+    name: "Rishi Raj Jain",
+    title: "Founder of LaunchFa.st (Guest Author)",
+    image: "rishi.jpeg",
+  },
+  pau_kraft: {
+    name: "Pau Kraft",
+    title: "Software Engineer @TeampilotAI (Guest Author)",
+    image: "pau_kraft.png",
+  },
+  jaydeep: {
+    name: "Jaydeep Das",
+    title: "Software Developer (Guest Author)",
+    image: "jaydeep.jpeg",
+  },
+  chris: {
+    name: "Chris Jayden",
+    title: "Software Engineer (Guest Author)",
+    image: "chris.jpg",
+  },
+  anshuman: {
+    name: "Anshuman Bhardwaj",
+    title: "Founder @docsly (Guest Author)",
+    image: "anshuman.png",
+  },
+  anish: {
+    name: "Anish Pallati",
+    title: "Software Developer (Guest Author)",
+    image: "anish.jpeg",
+  },
+  cameron: {
+    name: "Cameron Youngblood",
+    title: "Software Developer (Guest Author)",
+    image: "cameron.jpeg",
+  },
+  asim: {
+    name: "Asim Hafeez",
+    title: "Software Developer (Guest Author)",
+    image: "asim.jpeg",
+  },
+  brace: {
+    name: "Brace Sproul",
+    title: "Software Developer (Guest Author)",
+    image: "brace.jpeg",
+  },
+  ruben: {
+    name: "Ruben de Groot",
+    title: "Software Developer (Guest Author)",
+    image: "ruben.jpeg",
+  },
+  meshan: {
+    name: "Meshan Khosla",
+    title: "Software Engineer",
+    image: "meshan.png",
+  },
+  murillo: {
+    name: "Murillo de Miranda",
+    title: "Software Developer (Guest Author)",
+    image: "murillo.png",
+  },
+  yusuf: {
+    name: "Yusuf Kimirti",
+    title: "Sofware Engineer @Upstash",
+    image: "yusuf.jpg",
+  },
+  valon: {
+    name: "Valon Januzaj",
+    title: "Software Developer (Guest Author)",
+    image: "valon.png",
+  },
+  mahesh: {
+    name: "Mahesh Saini",
+    title: "Software Developer (Guest Author)",
+    image: "mahesh.jpeg",
+  },
+  eray: {
+    name: "Eray Eroglu",
+    title: "Software Developer (Guest Author)",
+    image: "eray.jpeg",
+  },
+  oguzhantasimaz: {
+    name: "Oguzhan Tasimaz",
+    title: "Software Engineer",
+    url: "https://oguzhantasimaz.com",
+    image: "oguzhantasimaz.png",
+  },
+  brighton: {
+    name: "Brighton Mboya",
+    title: "Software Engineer (Guest Author)",
+    url: "https://twitter.com/brightonmboya",
+    image: "brighton.jpg",
+  },
 };
