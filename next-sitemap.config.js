4f5d672bc5e65364af2fb6b3c050d4dcd156852f
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-02-13 21:22:11 +0300

    add sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const SITE_URL = process.env.SITE_URL || "https://upstash.com"
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: SITE_URL,
+	generateIndexSitemap: true,
+	generateRobotsTxt: true,
+}
+
