d82f49e42a1f5b1dda0c0297bbad221a31fc3ab4
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-02-26 12:28:26 +0300

    static robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,25 +4,6 @@ const SITE_URL = process.env.SITE_URL || "https://upstash.com";
 module.exports = {
   siteUrl: SITE_URL,
   generateIndexSitemap: true,
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-	additionalSitemaps: ['https://upstash.com/docs/sitemap.xml'],
-    policies: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/*?",
-          "/*%",
-          "/v1*",
-          "/v2*",
-          "/help/",
-          "/oss/",
-          "/api/",
-          "/tags/",
-          "/devops/",
-        ],
-      },
-    ],
-  },
+  generateRobotsTxt: false,
+  
 };
