6bb64ab1fda4e5b69adbac9885b3c1168db79765
sancar <sancar@upstash.com>
2024-05-03 15:13:32 +0300

    Add Qstash Queue to the pricing page

--- src/components/pricing/qstash/compare-table.tsx
+++ src/components/pricing/qstash/compare-table.tsx
@@ -405,7 +405,7 @@ export default function CompareTable() {
 
         <tr>
           <th className="px-0 py-4 text-left font-normal text-white/60">
-            Max Schedule Count
+            Max Queue Count
           </th>
           {/**/}
           <td
@@ -419,6 +419,31 @@ export default function CompareTable() {
             className="bg-emerald-300/10 px-4 py-0"
           >
             <CompareValue type="number">100</CompareValue>
+          </td>
+          <td
+            hidden={isMobile ? !showEnterprise : false}
+            className="bg-white/3 px-4 py-0"
+          >
+            <CompareValue>Custom</CompareValue>
+          </td>
+        </tr>
+
+         <tr>
+          <th className="px-0 py-4 text-left font-normal text-white/60">
+            Max Queue Parallelism
+          </th>
+          {/**/}
+          <td
+            hidden={isMobile ? !showFree : false}
+            className="bg-white/3 px-4 py-0"
+          >
+            <CompareValue type="number">2</CompareValue>
+          </td>
+          <td
+            hidden={isMobile ? !showPayg : false}
+            className="bg-emerald-300/10 px-4 py-0"
+          >
+            <CompareValue type="number">10</CompareValue>
           </td>
           <td
             hidden={isMobile ? !showEnterprise : false}
