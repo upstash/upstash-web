6744969f2e140224ed4ca9ad9718d96fc4446a88
Josh <josh@upstash.com>
2024-07-22 12:59:05 +0200

    chore: compare pro2k to pro10k and optimize wording

--- src/components/pricing/redis/pricing-table.tsx
+++ src/components/pricing/redis/pricing-table.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/button";
 
 export default function PricingTable() {
   return (
-    <div className="grid gap-6 md:grid-cols-3">
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
       {/**/}
 
       {/* FREE */}
@@ -62,7 +62,7 @@ export default function PricingTable() {
 
         <div className="grow">
           <div className="text-white/80">
-            For use cases with bursting traffic.
+            Flexible pricing for variable traffic.
           </div>
         </div>
 
@@ -112,7 +112,7 @@ export default function PricingTable() {
 
         <div className="grow">
           <div className="text-white/80">
-            Unlimited commands for a fixed price.
+            Unlimited commands and enterprise-level performance for a fixed price.
           </div>
         </div>
 
@@ -121,6 +121,56 @@ export default function PricingTable() {
         <div>
           <p className="text-white/40">Max Commands Per Second</p>
           <p className="font-semibold">2,000</p>
+        </div>
+
+        <div>
+          <p className="text-white/40">Daily Command Limit</p>
+          <p className="font-semibold">Unlimited</p>
+        </div>
+
+        <hr className="w-2/3 border-0 border-b border-b-white/5" />
+
+        <div>
+          <Button
+            target="_self"
+            type="button"
+            hideIcon
+            href="https://console.upstash.com"
+            className="bg-zinc-50 font-medium text-zinc-950"
+          >
+            Start Now
+          </Button>
+        </div>
+      </div>
+
+      {/* PRO 10K */}
+
+      <div className="flex flex-col items-center gap-6 rounded-3xl bg-white/5 px-4 py-8">
+        <div className="grow">
+          <h4 className="mb-2 text-xl font-semibold text-emerald-400">
+            Pro 10K
+          </h4>
+
+          <h5 className="flex items-baseline text-3xl font-semibold">
+            $680
+            <span className="ml-1 text-base font-normal opacity-40">
+              / month
+            </span>
+          </h5>
+          <p className="text-white/40">+$100 ✕ read region</p>
+        </div>
+
+        <div className="grow">
+          <div className="text-white/80">
+            Everything in Pro2k at higher capacity, ideal for high-load applications.
+          </div>
+        </div>
+
+        <hr className="w-2/3 border-0 border-b border-b-white/5" />
+
+        <div>
+          <p className="text-white/40">Max Commands Per Second</p>
+          <p className="font-semibold">10,000</p>
         </div>
 
         <div>
