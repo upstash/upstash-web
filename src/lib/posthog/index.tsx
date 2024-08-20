da7442d0561435af2460243cc5d477711c575211
ogzhanolguncu <ogzhan11@gmail.com>
2024-08-20 15:26:29 +0300

    fix: add crossdomain

--- src/lib/posthog/index.tsx
+++ src/lib/posthog/index.tsx
@@ -20,6 +20,7 @@ if (typeof window !== "undefined") {
     person_profiles: "identified_only",
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     capture_pageleave: true,
+    cross_subdomain_cookie: true,
   });
 }
 
