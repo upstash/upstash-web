750fc922a57fdb2b574b6523bf0c9b799a9634a1
Adem ilter <ademilter@gmail.com>
2024-12-02 13:13:51 +0300

    style

--- src/components/bg.tsx
+++ src/components/bg.tsx
@@ -7,7 +7,7 @@ export default function Bg({ className }: HTMLProps<HTMLDivElement>) {
       className={cx(
         "absolute left-1/2 top-0 -z-10 -translate-x-1/2",
         "h-[200px] w-4/5 md:h-[400px]",
-        "bg-primary rounded-[100%] opacity-5 blur-[90px]",
+        "rounded-[100%] bg-primary opacity-10 blur-[90px] dark:opacity-5",
         "pointer-events-none",
         className,
       )}
