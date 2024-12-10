14da0bbd4a3afa37efd36852955ba0509c849f23
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-12-10 17:45:54 +0300

    update ui

--- src/components/cookie-consent-banner.tsx
+++ src/components/cookie-consent-banner.tsx
@@ -23,27 +23,25 @@ export const CookieConsentBanner = () => {
   if (!visible) return;
 
   return (
-    <div className="fixed bottom-4 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-full bg-emerald-400 py-1 pl-4 pr-1.5 text-sm text-black shadow-lg">
-      <span>
-        We use cookies to improve your experience.
+    <div className="fixed bottom-4 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-full bg-emerald-400 py-1.5 pl-4 pr-1.5 text-sm text-zinc-950 shadow-lg">
+      <span className="font-normal">
+        We use cookies to improve your experience. Read our
         <a
           href="https://upstash.com/trust/privacy.pdf"
           className="ml-1 underline"
         >
-          Read our privacy policy.
+          privacy policy.
         </a>
       </span>
-
       <div className="flex items-center gap-2.5">
         <button
           onClick={() => {
             setCookieConsent(true);
           }}
-          className="rounded-full bg-white px-3 py-1 text-xs transition-colors hover:bg-gray-100"
+          className="rounded-full bg-white px-3 py-1.5 text-xs transition-colors hover:bg-gray-100"
         >
           Accept
         </button>
-
         <button
           onClick={() => {
             setVisible(false);
