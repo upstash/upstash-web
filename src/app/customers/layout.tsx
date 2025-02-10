184b83f83caf8d5038b68e2c41b4ff82be9b9335
Adem ilter <ademilter@gmail.com>
2025-02-10 11:59:47 +0300

    fix typo

--- src/app/customers/layout.tsx
+++ src/app/customers/layout.tsx
@@ -5,7 +5,8 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 const title = "Upstash Customers";
-const description = ""; // TODO: add description
+const description =
+  "Upstash enables companies of all sizes to create at the moment of inspiration";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
