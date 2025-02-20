372f42d54f0982ac7ff4cb8a69c2ec5a999866e2
Adem ilter <ademilter@gmail.com>
2025-02-20 21:03:50 +0300

    fix mobile

--- src/components/home/fast/statistic.tsx
+++ src/components/home/fast/statistic.tsx
@@ -17,7 +17,7 @@ export default function FastStatistic() {
 
       <div
         className={cx(
-          "grid grid-cols-2 items-center gap-4 p-6 shadow-md md:flex-row",
+          "grid grid-cols-2 items-center gap-4 p-6 shadow-md md:grid-cols-4",
           "rounded-2xl bg-bg backdrop-blur dark:bg-bg-mute",
           "md:gap-12 md:rounded-4xl md:px-12 md:py-6",
         )}
