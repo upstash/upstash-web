35b5765e842df777b4afac75b2112b6cf50f3e1e
ytkimirti <yusuftaha9@gmail.com>
2025-03-14 13:17:27 +0100

    small review fix

--- src/components/pricing/product-toggle.tsx
+++ src/components/pricing/product-toggle.tsx
@@ -32,7 +32,7 @@ export default function ProductToggle({ product }: { product: Product }) {
               href={`/pricing${key}`}
               className={cx(
                 "relative flex cursor-pointer select-none items-center gap-0 sm:gap-1",
-                "rounded-lg px-2 py-1.5 transition sm:px-3",
+                "rounded-lg px-1.5 py-1.5 transition sm:px-3",
                 "hover:bg-bg-mute",
                 isActive && "text-black",
               )}
