7bc26e1dd83f68b3d3be6327a92c1928522cea72
ytkimirti <yusuftaha9@gmail.com>
2025-08-13 08:51:46 +0300

    feat: update customer testimonials

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -48,7 +48,7 @@ export default function CustomerPage() {
                 >
                   <span className="absolute inset-0 bg-gradient-to-br hover:from-emerald-500/20 hover:to-emerald-500/0" />
 
-                  <Logo className="h-8 max-w-full object-contain text-black dark:text-white" />
+                  <Logo className="h-9 max-w-[150px] object-contain text-black dark:text-white" />
 
                   <hr className="w-1/4 border-0 border-b border-black/5 dark:border-white/5" />
 
