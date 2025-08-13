8ac2dbbfadb73ddefca347d93c7aa780c2fa623b
ytkimirti <yusuftaha9@gmail.com>
2025-08-13 08:51:46 +0300

    feat: update customer testimonials

--- src/components/blog/blog-logos.tsx
+++ src/components/blog/blog-logos.tsx
@@ -1,19 +1,23 @@
 import { LogoBranch } from "../customers/logo-branch";
+import { LogoDropee } from "../customers/logo-dropee";
 import { LogoDubsh } from "../customers/logo-dubsh";
 import { LogoGail } from "../customers/logo-gail";
 import { LogoHumata } from "../customers/logo-humata";
 import { LogoMaker } from "../customers/logo-maker";
 import { LogoMarkprompt } from "../customers/logo-markprompt";
 import { LogoNiftykit } from "../customers/logo-niftykit";
+import { LogoOpenart } from "../customers/logo-openart";
 import { LogoTFashion } from "../customers/logo-tfashion";
 import { LogoZapier } from "../customers/logo-zapier";
 
 const blogLogos = {
   branch: LogoBranch,
   dub: LogoDubsh,
+  dropee: LogoDropee,
   humata: LogoHumata,
   maker: LogoMaker,
   markprompt: LogoMarkprompt,
+  openart: LogoOpenart,
   niftykit: LogoNiftykit,
   tfashion: LogoTFashion,
   zapier: LogoZapier,
