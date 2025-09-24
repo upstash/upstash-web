660872355c64f0573233d304e550829e795c22cc
CahidArda <cahidardaooz@hotmail.com>
2025-09-24 17:00:25 +0300

    feat: add prod pack card to qstash/workflow

--- src/components/pricing/qstash/enterprise.tsx
+++ src/components/pricing/qstash/enterprise.tsx
@@ -5,42 +5,46 @@ import * as React from "react";
 
 export default function PricingTableEnterprise() {
   return (
-    <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-bg-mute bg-bg-mute px-10 py-8 md:flex-row md:px-16 md:py-10 md:text-left">
-      <div>
-        <h4 className="text-2xl font-semibold text-primary-text">Enterprise</h4>
-        <p className="">For businesses with advanced needs.</p>
+    <div className="grid grid-cols-1 items-center gap-4 rounded-3xl border-2 border-emerald-200 bg-gradient-to-r from-emerald-50 to-white px-10 py-8 text-emerald-800 md:grid-cols-2 md:flex-row md:px-16 md:py-10 md:text-left dark:border-emerald-600/20 dark:from-emerald-950/10 dark:to-emerald-900/10 dark:text-emerald-200">
+      <div className="">
+        <h4 className="text-xl font-semibold">Enterprise</h4>
+        <p className="opacity-80">For businesses with advanced needs.</p>
 
         <Button
           asChild
           variant="secondary"
-          className="-ml-1 mt-4 hidden md:inline-flex"
+          className="mt-4 hidden bg-emerald-200 text-emerald-800 transition-colors hover:bg-white md:inline-flex dark:bg-emerald-50 dark:text-emerald-950"
         >
           <Link href="/enterprise">Learn More</Link>
         </Button>
       </div>
 
-      <div className="mt-6 md:ml-24 md:mt-0">
+      <div className="justify-self-center pl-0 text-left md:justify-self-start lg:pl-8">
         <ul className="space-y-2">
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />{" "}
+            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
             100M+ messages daily
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />{" "}
+            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
             Unlimited bandwidth
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />{" "}
+            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
             Professional support with SLA
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />{" "}
+            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
             Dedicated resources for isolation
           </li>
         </ul>
       </div>
 
-      <Button asChild variant="secondary" className="-ml-1 mt-6 md:hidden">
+      <Button
+        asChild
+        variant="secondary"
+        className="inline-flex justify-self-center bg-emerald-200 text-emerald-800 md:hidden"
+      >
         <Link href="/enterprise">Learn More</Link>
       </Button>
     </div>
