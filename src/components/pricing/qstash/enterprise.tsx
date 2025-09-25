4ce9d9f050aac1af84367df0c97d25d945be7a01
CahidArda <cahidardaooz@hotmail.com>
2025-09-25 14:43:30 +0300

    feat: change enterprise color to amber from green

--- src/components/pricing/qstash/enterprise.tsx
+++ src/components/pricing/qstash/enterprise.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 
 export default function PricingTableEnterprise() {
   return (
-    <div className="grid grid-cols-1 items-center gap-4 rounded-3xl border-2 border-emerald-200 bg-gradient-to-r from-emerald-50 to-white px-10 py-8 text-emerald-800 md:grid-cols-2 md:flex-row md:px-16 md:py-10 md:text-left dark:border-emerald-600/20 dark:from-emerald-950/10 dark:to-emerald-900/10 dark:text-emerald-200">
+    <div className="grid grid-cols-1 items-center gap-4 rounded-3xl border-2 border-amber-200 bg-gradient-to-r from-amber-50 to-white px-10 py-8 text-amber-800 md:grid-cols-2 md:flex-row md:px-16 md:py-10 md:text-left dark:border-amber-600/20 dark:from-amber-950/10 dark:to-amber-900/10 dark:text-amber-200">
       <div className="">
         <h4 className="text-xl font-semibold">Enterprise</h4>
         <p className="opacity-80">For businesses with advanced needs.</p>
@@ -13,7 +13,7 @@ export default function PricingTableEnterprise() {
         <Button
           asChild
           variant="secondary"
-          className="mt-4 hidden bg-emerald-200 text-emerald-800 transition-colors hover:bg-white md:inline-flex dark:bg-emerald-50 dark:text-emerald-950"
+          className="mt-4 hidden bg-amber-200 text-amber-800 transition-colors hover:bg-white md:inline-flex dark:bg-amber-50 dark:text-amber-950"
         >
           <Link href="/enterprise">Learn More</Link>
         </Button>
@@ -22,19 +22,19 @@ export default function PricingTableEnterprise() {
       <div className="justify-self-center pl-0 text-left md:justify-self-start lg:pl-8">
         <ul className="space-y-2">
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
+            <IconCircleCheckFilled size={20} className="fill-amber-600" />
             100M+ messages daily
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
+            <IconCircleCheckFilled size={20} className="fill-amber-600" />
             Unlimited bandwidth
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
+            <IconCircleCheckFilled size={20} className="fill-amber-600" />
             Professional support with SLA
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-emerald-600" />
+            <IconCircleCheckFilled size={20} className="fill-amber-600" />
             Dedicated resources for isolation
           </li>
         </ul>
@@ -43,7 +43,7 @@ export default function PricingTableEnterprise() {
       <Button
         asChild
         variant="secondary"
-        className="inline-flex justify-self-center bg-emerald-200 text-emerald-800 md:hidden"
+        className="inline-flex justify-self-center bg-amber-200 text-amber-800 md:hidden"
       >
         <Link href="/enterprise">Learn More</Link>
       </Button>
--- src/components/pricing/vector/enterprise.tsx
+++ src/components/pricing/vector/enterprise.tsx
@@ -7,7 +7,7 @@ export default function PricingTableEnterprise() {
   return (
     <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-bg-mute bg-bg-mute px-10 py-8 md:flex-row md:px-16 md:py-10 md:text-left">
       <div>
-        <h4 className="text-2xl font-semibold text-primary-text">Enterprise</h4>
+        <h4 className="!text-amber-500 text-2xl font-semibold text-primary-text">Enterprise</h4>
         <p className="">For businesses with advanced needs.</p>
 
         <Button
@@ -22,19 +22,19 @@ export default function PricingTableEnterprise() {
       <div className="mt-6 md:ml-24 md:mt-0">
         <ul className="space-y-2">
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />
+            <IconCircleCheckFilled size={20} className="fill-amber-500" />
             Professional support with SLA
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />
+            <IconCircleCheckFilled size={20} className="fill-amber-500" />
             Scales over a billion of vectors
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />
+            <IconCircleCheckFilled size={20} className="fill-amber-500" />
             Custom configurations
           </li>
           <li className="flex items-center gap-1">
-            <IconCircleCheckFilled size={20} className="fill-primary-text" />
+            <IconCircleCheckFilled size={20} className="fill-amber-500" />
             Advanced security options
           </li>
         </ul>
