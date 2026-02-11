22acbef483034e82b761f6e852f29b4b80f57909
ytkimirti <yusuftaha9@gmail.com>
2026-02-11 11:31:41 +0300

    fix: investors section

--- src/utils/investors.ts
+++ src/utils/investors.ts
@@ -79,7 +79,7 @@ export const INVESTORS = [
   },
   {
     name: "Morgan Dioli",
-    title: "Investor/Advisor, Ravix Group",
+    title: "Investor/Advisor",
     src: "/investors/morgan-dioli.jpeg",
     url: "https://www.linkedin.com/in/morgan-dioli-author-cfo",
   },
