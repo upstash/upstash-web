3ff1b21575a570f659957158f2e8376ad7e04c3d
Josh <josh@upstash.com>
2026-05-29 14:36:08 +0200

    fix: player overflow

--- src/components/post/mux-video-player.tsx
+++ src/components/post/mux-video-player.tsx
@@ -2,7 +2,7 @@ import MuxPlayer from "@mux/mux-player-react";
 
 export const MuxVideoPlayer = ({ id }: { id: string }) => {
   return (
-    <div className="-mx-5 overflow-hidden rounded-xl md:-mx-6">
+    <div className="overflow-hidden rounded-xl">
       <MuxPlayer
         playbackId={id}
         metadataVideoTitle="Placeholder (optional)"
@@ -11,6 +11,7 @@ export const MuxVideoPlayer = ({ id }: { id: string }) => {
         secondary-color="#000000"
         accent-color="#4ae8a3"
         muted
+        style={{ display: "block", width: "100%", aspectRatio: "16 / 9" }}
       />
     </div>
   );
